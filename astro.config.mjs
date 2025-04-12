fc737a66c426598c9442909492e0e8353306f595
estebanguzzo <estebanguzzo@gmail.com>
2025-04-12 04:35:52 +0000

    Configure CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://epirex.github.io',
-  base: '/grafica/',
-  outDir: './dist',
-  build: {
-    format: 'directory'
-  }
+  site: 'https://estebanguzzo.com.ar',
+  base: '/grafica'
 });
