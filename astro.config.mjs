d06af5bf6df568e52dfeed2fb936327b0ac5281b
estebanguzzo <estebanguzzo@gmail.com>
2025-04-12 04:56:22 +0000

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import astroIcon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://estebanguzzo.com.ar',
-  base: '/',
+  base: '/grafica',
   integrations: [astroIcon()],
   outDir: './dist',
   build: {
