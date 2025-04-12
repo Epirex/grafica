2e908c78546b63a446542ec280ca51f4e276df01
estebanguzzo <estebanguzzo@gmail.com>
2025-04-12 05:01:44 +0000

    Updates

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Esteban Guzzo';
-export const SITE_DESCRIPTION = 'Portafolio de Esteban Guzzo';
